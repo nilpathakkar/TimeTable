dc1be381510f1903e358c5ef0db73a4f07e49ff3
chirag thakkar <nilpathakkar1984@gmail.com>
2022-05-03 22:41:37 +0100

    TimeTable2

---
+++ src/Timestable2.java
@@ -0,0 +1,9 @@
+public class Timestable2 {
+    public static void main(String[] args) {
+        int a=1;  int b=2;
+        while(a<=10){
+            System.out.println(a+"*"+b+"="+a*b);
+            a++;
+        }
+    }
+}
